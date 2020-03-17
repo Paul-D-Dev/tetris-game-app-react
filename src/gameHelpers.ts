9899d3d7ec3123b84400b2d844ca3f2b9c283e05
Paul DAO <dao.van.paul@gmail.com>
2020-03-17 15:27:02 +0100

    fix bug line

--- src/gameHelpers.ts
+++ src/gameHelpers.ts
@@ -10,20 +10,23 @@ export const createStage = () => (
 export const checkCollision = (player: { tetromino: string | any[]; pos: { y: number; x: number; }; }, stage: { [x: string]: { [x: string]: string[]; }; }, { x: moveX, y: moveY}: any) => {
     for (let y = 0; y < player.tetromino.length; y+=1) {
 
-        // 1. Check taht we're on an actual Tetromino cell
         for (let x = 0; x < player.tetromino[y].length; x++) {
-            if(
-            // 2. Check taht our move is inside the game areas height (y)
-            // We shoudn't go through the bottom of the play area
-                !stage[y + player.pos.y + moveY] ||
-                // 3. Check that our move is inside the game areas width (x)
-                !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-                // 4. Chaeck taht the cell we're moving to isn't to clear
-                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
-            ) {
-                return true;
+            // 1. Check taht we're on an actual Tetromino cell
+            if(player.tetromino[y][x] !== 0){
+                if(
+                    // 2. Check taht our move is inside the game areas height (y)
+                    // We shoudn't go through the bottom of the play area
+                    !stage[y + player.pos.y + moveY] ||
+                    // 3. Check that our move is inside the game areas width (x)
+                    !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                    // 4. Chaeck taht the cell we're moving to isn't to clear
+                    stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                    ) {
+                        return true;
+                    }
             }
-            
         }
     }
 }
+            
+
